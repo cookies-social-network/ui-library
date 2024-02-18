1920e5155f3df4f031dbc94340eaa8259c88efb9
miirsery <miirsery.main@gmail.com>
2024-02-18 18:12:24 +0700

    update css

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 import dts from 'vite-plugin-dts'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +11,8 @@ export default defineConfig({
     dts({
       insertTypesEntry: true,
       rollupTypes: true,
-    })
+    }),
+    libInjectCss(),
   ],
   build: {
     lib: {
