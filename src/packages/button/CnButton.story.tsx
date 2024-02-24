57a379de1b239f75958d8a373e8ec796e3623207
miirsery <miirsery.main@gmail.com>
2024-02-25 02:37:00 +0700

    UL-1 | Верстка кнопки светлой темы

--- src/packages/button/CnButton.story.tsx
+++ src/packages/button/CnButton.story.tsx
@@ -1,5 +1,6 @@
 import {CnButton} from "./CnButton";
 import {Meta, StoryObj} from "@storybook/react";
+import {IconPlus} from "../../icons";
 
 const meta = {
     title: 'CnButton',
@@ -10,6 +11,7 @@ const meta = {
     tags: ['autodocs'],
 } satisfies Meta<typeof CnButton>
 
+
 export default meta
 
 type Story = StoryObj<typeof meta>
@@ -18,6 +20,7 @@ export const Primary: Story = {
     args: {
         type: 'button',
         children: 'Label',
+      prefixIcon: <IconPlus />,
         appearance: 'filled'
     },
 }
@@ -27,12 +30,78 @@ export const PrimaryDisabled: Story = {
     children: 'Label',
     appearance: 'filled',
     disabled: true,
+    prefixIcon: <IconPlus />,
   }
+}
+
+export const PrimaryWithoutIcon: Story = {
+  args: {
+    type: 'button',
+    children: 'Label',
+    appearance: 'filled'
+  },
 }
 
 export const Outlined: Story = {
     args: {
         children: 'Label',
-        appearance: 'outlined'
+        appearance: 'outlined',
+      prefixIcon: <IconPlus />,
     },
 };
+
+export const OutlinedDisabled: Story = {
+  args: {
+    children: 'Label',
+    appearance: 'outlined',
+    disabled: true,
+    prefixIcon: <IconPlus />,
+  },
+};
+
+export const Text: Story = {
+  args: {
+    children: 'Label',
+    appearance: 'text',
+  },
+};
+
+export const TextDisabled: Story = {
+  args: {
+    children: 'Label',
+    appearance: 'text',
+    disabled: true,
+  },
+};
+
+
+export const Elevated: Story = {
+  args: {
+    children: 'Label',
+    appearance: 'elevated',
+  },
+};
+
+export const ElevatedDisabled: Story = {
+  args: {
+    children: 'Label',
+    appearance: 'elevated',
+    disabled: true,
+  },
+};
+
+export const Tonal: Story = {
+  args: {
+    children: 'Label',
+    appearance: 'tonal',
+  },
+};
+
+export const TonalDisabled: Story = {
+  args: {
+    children: 'Label',
+    appearance: 'tonal',
+    disabled: true,
+  },
+};
+
